89265f2ebfe62376d5f041e42f0959c17cf1f06c
SolsticeDante <solsticedante@gmail.com>
2019-04-20 21:05:23 +0800

    removed unwanted code

--- https:/github.com/Zhaohong99/50.003_1D_2019.git/websiteStyleStyle/login/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label='Username:', max_length=100,
-                               widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label='Password:', max_length=100,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
